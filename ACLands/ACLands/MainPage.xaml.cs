05d08d6da7e78b00a0d917909160fb181125934c
tonyuste <tonyusteruiz@gmail.com>
2018-03-06 22:20:13 +0100

    ActInicio

---
+++ ACLands/ACLands/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ACLands
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
