3ee5ca02ae29ff5f67df8f157c4e753e47330fc2
tonyuste <tonyusteruiz@gmail.com>
2018-03-08 09:20:31 +0100

    LoginPage

---
+++ ACLands/ACLands/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ACLands.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
