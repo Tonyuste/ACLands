1b44a61d6bc88b0bb22670b791ab763bcb002ee6
tonyuste <tonyusteruiz@gmail.com>
2018-03-08 11:31:34 +0100

    MainViewModel

--- ACLands/ACLands/App.xaml.cs
+++ ACLands/ACLands/App.xaml.cs
@@ -1,34 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
-namespace ACLands
+﻿namespace ACLands
 {
+    using Xamarin.Forms;
+
+    using Views;
+
 	public partial class App : Application
 	{
-		public App ()
-		{
-			InitializeComponent();
+		#region Constructors
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new ACLands.MainPage();
-		}
+            MainPage = new NavigationPage(new LoginPage());
+        }
+		#endregion
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+		#region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+		#endregion
 	}
 }
---
+++ ACLands/ACLands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,24 @@
+﻿namespace ACLands.Infrastructure
+{
+    using ViewModels;
+
+    public class InstanceLocator
+    {
+
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+
+    }
+}
