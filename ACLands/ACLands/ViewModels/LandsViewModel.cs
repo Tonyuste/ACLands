b294fe18a177f3e68964641ec8b6d9897b5b6682
tonyuste <tonyusteruiz@gmail.com>
2018-03-12 18:57:53 +0100

    Service

--- ACLands/ACLands/ViewModels/LandsViewModel.cs
+++ ACLands/ACLands/ViewModels/LandsViewModel.cs
@@ -1,6 +1,73 @@
 ﻿namespace ACLands.ViewModels
 {
-    public class LandsViewModel
+    using System.Collections.ObjectModel;
+    using Services;
+    using Models;
+    using Xamarin.Forms;
+    using System.Collections.Generic;
+
+    public class LandsViewModel : BaseViewModel
     {
+        #region Services
+        private ApiService apiService;
+        #endregion
+
+        #region Attributes
+        private ObservableCollection<Land> lands;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Land> Lands
+        {
+            get { return this.lands; }
+            set { SetValue(ref this.lands, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public LandsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadLands();
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadLands()
+        {
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                     "Error",
+                     connection.Message,
+                     "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+
+                return;
+            }
+
+            var response = await this.apiService.GetList<Land>(
+                "http://restcountries.eu",
+                 "/rest",
+                 "/v2/all");
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                     "Error",
+                     response.Message,
+                     "Accept");
+                
+                await Application.Current.MainPage.Navigation.PopAsync();
+
+                return;
+            }
+
+            var list = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(list);
+        }
+        #endregion
     }
 }
