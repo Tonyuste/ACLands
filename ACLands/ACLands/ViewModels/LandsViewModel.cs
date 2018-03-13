e6582c5254a14a8d5946fecde5698472a31694ca
tonyuste <tonyusteruiz@gmail.com>
2018-03-13 16:36:44 +0100

    Search

--- ACLands/ACLands/ViewModels/LandsViewModel.cs
+++ ACLands/ACLands/ViewModels/LandsViewModel.cs
@@ -7,6 +7,8 @@
     using System.Collections.Generic;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Linq;
 
     public class LandsViewModel : BaseViewModel
     {
@@ -17,6 +19,8 @@
         #region Attributes
         private ObservableCollection<Land> lands;
         private bool isRefreshing;
+        private string filter;
+        private List<Land> landsList;
         #endregion
 
         #region Properties
@@ -30,6 +34,16 @@
         {
             get { return this.isRefreshing; }
             set { SetValue(ref this.isRefreshing, value); }
+        }
+
+        public string Filter
+        {
+            get { return this.filter; }
+            set
+            {
+                SetValue(ref this.filter, value);
+                this.Search();
+            }
         }
         #endregion
 
@@ -80,8 +94,8 @@
                 return;
             }
 
-            var list = (List<Land>)response.Result;
-            this.Lands = new ObservableCollection<Land>(list);
+            this.landsList = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(this.landsList);
             this.IsRefreshing = false;
         }
         #endregion
@@ -92,6 +106,26 @@
             get
             {
                 return new RelayCommand(LoadLands);
+            }
+        }
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+        }
+
+        private void Search()
+        {
+            if (string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<Land>(this.landsList);
+            }
+            else
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landsList.Where(l => l.Name.ToLower().Contains(this.Filter.ToLower())));
             }
         }
         #endregion
