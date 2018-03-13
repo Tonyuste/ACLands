28884231a3c15d671f2db385876e939734736839
tonyuste <tonyusteruiz@gmail.com>
2018-03-13 19:14:49 +0100

    Pestañas

---
+++ ACLands/ACLands/Views/LandTabbedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ACLands.Views
+{
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LandTabbedPage : TabbedPage
+	{
+		public LandTabbedPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
